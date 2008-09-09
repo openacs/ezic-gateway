e3872d9d081dfc26203bc7864e7ae51d3233633e
Torben Brosten <torben@dekka.net>
2008-09-09 08:39:12 +0000

    updating report one transaction response details

--- lib/one.adp
+++ lib/one.adp
@@ -1,1 +1,1 @@
-@response_reason_text@ @avs_text@
+@response_reason_text@ @avs_text@ @code_text@
--- lib/one.tcl
+++ lib/one.tcl
@@ -10,6 +10,17 @@ set admin_p [ad_permission_p $package_id admin]
 # get transaction log comments. 
 db_1row get_transaction_comments "select response_reason_text, avs_code
       from ezic_gateway_result_log 
-      where transaction_id = :transaction_id and amount = :amount and (substr(txn_attempted_type,1,9) = 'AUTH_ONLY' or substr(txn_attempted_type,1,12) = 'AUTH_CAPTURE')"
+      where transaction_id = :transaction_id and amount = :amount and (substr(txn_attempted_type,1,9) = 'AUTH_ONLY' or substr(txn_attempted_type,1,12) = 'AUTH_CAPTURE') and response_reason_text is not null and avs_code is not null order by txn_attempted_time desc limit 1"
 
-set avs_text [ezic_gateway.expand_avs $avs_code]
+if { [info exists response_reason_text] && [info exists avs_code] } {
+    # decode left most avs_code. Second character is card CVV2/CVC2/CID code response
+    set avs_text [ezic_gateway.expand_avs [string range $avs_code 0 0]]
+    set code_text "CID: [string range $avs_code 1 1]"
+} else {
+    set response_reason_text ""
+    set avs_text ""
+    set code_text ""
+}
+
+
+
